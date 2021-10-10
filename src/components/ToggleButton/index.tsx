5f5a4331f9a21a3caf02fe582e0ef8efa153e3b6
denniman <dennimanadams@gmail.com>
2021-10-10 22:52:10 +0100

    seperate styles

--- src/components/ToggleButton/index.tsx
+++ src/components/ToggleButton/index.tsx
@@ -1,5 +1,4 @@
-import styled from 'styled-components';
-import Theme from '../../components/Theme';
+import { SwitchWrapper } from './styles';
 
 export const ToggleButton = () => {
   return (
@@ -11,60 +10,3 @@ export const ToggleButton = () => {
     </SwitchWrapper>
   );
 };
-
-const SwitchWrapper = styled.div`
-  .switch {
-    position: relative;
-    display: inline-block;
-    width: 5.5rem;
-    height: 3rem;
-  }
-
-  .switch input {
-    opacity: 0;
-    width: 0;
-    height: 0;
-  }
-
-  .slider {
-    position: absolute;
-    cursor: pointer;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background-color: #f4f4f4;
-    -webkit-transition: 0.4s;
-    transition: 0.4s;
-  }
-
-  .slider:before {
-    position: absolute;
-    content: '';
-    height: 23px;
-    width: 23px;
-    left: 4px;
-    bottom: 4px;
-    background-color: #fff;
-    -webkit-transition: 0.4s;
-    transition: 0.4s;
-  }
-
-  input:checked + .slider {
-    background-color: ${Theme.colorPallete.themeBlue};
-  }
-
-  input:checked + .slider:before {
-    -webkit-transform: translateX(26px);
-    -ms-transform: translateX(26px);
-    transform: translateX(26px);
-  }
-
-  .slider.round {
-    border-radius: 34px;
-  }
-
-  .slider.round:before {
-    border-radius: 50%;
-  }
-`;
